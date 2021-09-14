ef4f3abf8978765fa79bd6cc1cff06157e4292f9
Stephen Gruzin <stephengrzn@gmail.com>
2021-09-14 13:48:46 -0700

    Experimenting with different libraries

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const Troubadour = require('troubadour');
+const troubadour = new Troubadour('vlc');
+
+troubadour.on('start', () => {
+    // Do something here when the audio starts playing
+  });
+  
+  troubadour.on('end', () => {
+    // Do something here when the audio finishes playing
+  });
+  
+  troubadour.play('./sample.mp3');
